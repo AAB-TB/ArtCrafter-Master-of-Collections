1c1b4e67e4d89a3e61f55dbc741bbea7be0844b6
Alvin Alamgir berglund <alvinalamgirberglund@gmail.com>
2023-10-17 16:30:29 +0200

    database query

---
+++ SQLQuery5.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Category
+(
+    CategoryID INT PRIMARY KEY IDENTITY(1,1),
+    CategoryName NVARCHAR(MAX)
+);
+
+CREATE TABLE CollectionItem
+(
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(MAX),
+    PurchaseDate DATE,
+    PurchasePrice DECIMAL(18, 2),
+    PurchaseLocation NVARCHAR(MAX),
+    SaleDate DATE,
+    SalePrice DECIMAL(18, 2),
+    SaleLocation NVARCHAR(MAX),
+    FrontImageFilePath NVARCHAR(MAX),
+    BackImageFilePath NVARCHAR(MAX),
+    Description NVARCHAR(MAX),
+    CategoryID INT FOREIGN KEY REFERENCES Category(CategoryID)
+);
