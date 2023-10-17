444751ab7f032c6c11a34f4d7e78f187698a9649
Alvin Alamgir berglund <alvinalamgirberglund@gmail.com>
2023-10-17 16:28:58 +0200

    deleted sql

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -50,6 +50,10 @@
             tabPage2 = new TabPage();
             pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            label2 = new Label();
+            dateTimePicker2 = new DateTimePicker();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             tabControl1.SuspendLayout();
@@ -68,7 +72,7 @@
             panel1.Controls.Add(pictureBox1);
             panel1.Location = new Point(12, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(270, 823);
+            panel1.Size = new Size(270, 921);
             panel1.TabIndex = 1;
             // 
             // addBtn
@@ -109,7 +113,7 @@
             // 
             textBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             textBox1.ForeColor = Color.DarkGreen;
-            textBox1.Location = new Point(409, 408);
+            textBox1.Location = new Point(401, 398);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "Föremål Namn";
             textBox1.Size = new Size(549, 38);
@@ -119,7 +123,7 @@
             // 
             textBox3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             textBox3.ForeColor = Color.DarkGreen;
-            textBox3.Location = new Point(409, 545);
+            textBox3.Location = new Point(404, 579);
             textBox3.Name = "textBox3";
             textBox3.PlaceholderText = "Inköpspris";
             textBox3.Size = new Size(549, 38);
@@ -129,7 +133,7 @@
             // 
             textBox4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             textBox4.ForeColor = Color.DarkGreen;
-            textBox4.Location = new Point(409, 602);
+            textBox4.Location = new Point(401, 752);
             textBox4.Name = "textBox4";
             textBox4.PlaceholderText = "Försäljningspris";
             textBox4.Size = new Size(549, 38);
@@ -140,7 +144,7 @@
             comboBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             comboBox1.ForeColor = Color.DarkGreen;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(409, 664);
+            comboBox1.Location = new Point(401, 806);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(549, 39);
             comboBox1.TabIndex = 7;
@@ -150,7 +154,7 @@
             // 
             textBox5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             textBox5.ForeColor = Color.DarkGreen;
-            textBox5.Location = new Point(409, 778);
+            textBox5.Location = new Point(394, 892);
             textBox5.Name = "textBox5";
             textBox5.PlaceholderText = "Lägg till Ny kategori";
             textBox5.Size = new Size(319, 38);
@@ -161,7 +165,7 @@
             itemName.AutoSize = true;
             itemName.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             itemName.ForeColor = Color.DarkGreen;
-            itemName.Location = new Point(341, 729);
+            itemName.Location = new Point(337, 848);
             itemName.Name = "itemName";
             itemName.Size = new Size(708, 31);
             itemName.TabIndex = 9;
@@ -172,7 +176,7 @@
             button1.BackColor = Color.WhiteSmoke;
             button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             button1.ForeColor = Color.DarkGreen;
-            button1.Location = new Point(743, 778);
+            button1.Location = new Point(741, 891);
             button1.Name = "button1";
             button1.Size = new Size(215, 38);
             button1.TabIndex = 10;
@@ -184,7 +188,7 @@
             dateTimePicker1.CalendarFont = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             dateTimePicker1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             dateTimePicker1.Format = DateTimePickerFormat.Short;
-            dateTimePicker1.Location = new Point(409, 485);
+            dateTimePicker1.Location = new Point(404, 525);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(549, 38);
             dateTimePicker1.TabIndex = 11;
@@ -195,7 +199,7 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.DarkGreen;
-            label1.Location = new Point(409, 451);
+            label1.Location = new Point(397, 491);
             label1.Name = "label1";
             label1.Size = new Size(156, 31);
             label1.TabIndex = 12;
@@ -209,12 +213,12 @@
             // 
             textBox2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             textBox2.ForeColor = Color.DarkGreen;
-            textBox2.Location = new Point(1131, 54);
+            textBox2.Location = new Point(1131, 47);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
             textBox2.PlaceholderText = "Föremål beskrivning";
             textBox2.ScrollBars = ScrollBars.Vertical;
-            textBox2.Size = new Size(580, 781);
+            textBox2.Size = new Size(580, 886);
             textBox2.TabIndex = 13;
             // 
             // updateBtn
@@ -226,7 +230,7 @@
             updateBtn.Name = "updateBtn";
             updateBtn.Size = new Size(239, 47);
             updateBtn.TabIndex = 3;
-            updateBtn.Text = "Spara";
+            updateBtn.Text = "Uppdetera";
             updateBtn.UseVisualStyleBackColor = false;
             // 
             // tabControl1
@@ -234,7 +238,7 @@
             tabControl1.Appearance = TabAppearance.Buttons;
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(409, 26);
+            tabControl1.Location = new Point(397, 12);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
             tabControl1.Size = new Size(549, 376);
@@ -280,11 +284,57 @@
             pictureBox3.TabIndex = 0;
             pictureBox3.TabStop = false;
             // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox6.ForeColor = Color.DarkGreen;
+            textBox6.Location = new Point(401, 633);
+            textBox6.Name = "textBox6";
+            textBox6.PlaceholderText = "Försäljningsplats";
+            textBox6.Size = new Size(549, 38);
+            textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox7.ForeColor = Color.DarkGreen;
+            textBox7.Location = new Point(401, 452);
+            textBox7.Name = "textBox7";
+            textBox7.PlaceholderText = "Inköpsplats";
+            textBox7.Size = new Size(549, 38);
+            textBox7.TabIndex = 16;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.DarkGreen;
+            label2.Location = new Point(400, 674);
+            label2.Name = "label2";
+            label2.Size = new Size(214, 31);
+            label2.TabIndex = 18;
+            label2.Text = "Försäljningsdatum";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.CalendarFont = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            dateTimePicker2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            dateTimePicker2.Format = DateTimePickerFormat.Short;
+            dateTimePicker2.Location = new Point(401, 708);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(549, 38);
+            dateTimePicker2.TabIndex = 17;
+            dateTimePicker2.Value = new DateTime(2023, 10, 17, 14, 34, 1, 0);
+            // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1739, 847);
+            ClientSize = new Size(1739, 945);
+            Controls.Add(label2);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
             Controls.Add(tabControl1);
             Controls.Add(textBox2);
             Controls.Add(label1);
@@ -333,5 +383,9 @@
         private PictureBox pictureBox2;
         private TabPage tabPage2;
         private PictureBox pictureBox3;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label2;
+        private DateTimePicker dateTimePicker2;
     }
 }
--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -39,14 +39,22 @@
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
             tabPage2 = new TabPage();
-            dateTimePicker1 = new DateTimePicker();
-            textBox4 = new TextBox();
-            textBox3 = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
             pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
+            groupBox1 = new GroupBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            textBox7 = new TextBox();
+            groupBox2 = new GroupBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label8 = new Label();
+            label9 = new Label();
+            dateTimePicker3 = new DateTimePicker();
+            textBox8 = new TextBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             tabControl1.SuspendLayout();
@@ -54,6 +62,8 @@
             tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -118,19 +128,19 @@
             // 
             textBox2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             textBox2.ForeColor = Color.DarkGreen;
-            textBox2.Location = new Point(304, 572);
+            textBox2.Location = new Point(379, 572);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
             textBox2.PlaceholderText = "Föremål beskrivning";
             textBox2.ScrollBars = ScrollBars.Vertical;
-            textBox2.Size = new Size(1399, 263);
+            textBox2.Size = new Size(1272, 263);
             textBox2.TabIndex = 14;
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             textBox1.ForeColor = Color.DarkGreen;
-            textBox1.Location = new Point(644, 25);
+            textBox1.Location = new Point(379, 22);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "Föremål Namn";
             textBox1.Size = new Size(575, 38);
@@ -141,7 +151,7 @@
             tabControl1.Appearance = TabAppearance.Buttons;
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(644, 78);
+            tabControl1.Location = new Point(379, 75);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
             tabControl1.Size = new Size(575, 488);
@@ -169,70 +179,6 @@
             tabPage2.Text = "Baksida";
             tabPage2.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.CalendarFont = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            dateTimePicker1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            dateTimePicker1.Format = DateTimePickerFormat.Short;
-            dateTimePicker1.Location = new Point(1440, 227);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(211, 38);
-            dateTimePicker1.TabIndex = 19;
-            dateTimePicker1.Value = new DateTime(2023, 10, 17, 14, 34, 1, 0);
-            // 
-            // textBox4
-            // 
-            textBox4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            textBox4.ForeColor = Color.DarkGreen;
-            textBox4.Location = new Point(1440, 344);
-            textBox4.Name = "textBox4";
-            textBox4.PlaceholderText = "Försäljningspris";
-            textBox4.Size = new Size(211, 38);
-            textBox4.TabIndex = 18;
-            // 
-            // textBox3
-            // 
-            textBox3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            textBox3.ForeColor = Color.DarkGreen;
-            textBox3.Location = new Point(1440, 287);
-            textBox3.Name = "textBox3";
-            textBox3.PlaceholderText = "Inköpspris";
-            textBox3.Size = new Size(211, 38);
-            textBox3.TabIndex = 17;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = Color.DarkGreen;
-            label1.Location = new Point(1250, 233);
-            label1.Name = "label1";
-            label1.Size = new Size(156, 31);
-            label1.TabIndex = 20;
-            label1.Text = "Inköpsdatum";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.ForeColor = Color.DarkGreen;
-            label2.Location = new Point(1255, 290);
-            label2.Name = "label2";
-            label2.Size = new Size(126, 31);
-            label2.TabIndex = 21;
-            label2.Text = "Inköpspris";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.ForeColor = Color.DarkGreen;
-            label3.Location = new Point(1250, 347);
-            label3.Name = "label3";
-            label3.Size = new Size(184, 31);
-            label3.TabIndex = 22;
-            label3.Text = "Försäljningspris";
-            // 
             // pictureBox2
             // 
             pictureBox2.Dock = DockStyle.Fill;
@@ -253,17 +199,175 @@
             pictureBox3.TabIndex = 0;
             pictureBox3.TabStop = false;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(dateTimePicker2);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox1.ForeColor = Color.DarkGreen;
+            groupBox1.Location = new Point(1020, 356);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(617, 189);
+            groupBox1.TabIndex = 27;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Försäljnings Historia";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.DarkGreen;
+            label3.Location = new Point(13, 39);
+            label3.Name = "label3";
+            label3.Size = new Size(196, 31);
+            label3.TabIndex = 31;
+            label3.Text = "Försäljningsplats";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox4.ForeColor = Color.DarkGreen;
+            textBox4.Location = new Point(227, 36);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Försäljningsplats";
+            textBox4.Size = new Size(343, 38);
+            textBox4.TabIndex = 30;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.DarkGreen;
+            label6.Location = new Point(13, 136);
+            label6.Name = "label6";
+            label6.Size = new Size(184, 31);
+            label6.TabIndex = 29;
+            label6.Text = "Försäljningspris";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = Color.DarkGreen;
+            label7.Location = new Point(13, 86);
+            label7.Name = "label7";
+            label7.Size = new Size(214, 31);
+            label7.TabIndex = 28;
+            label7.Text = "Försäljningsdatum";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.CalendarFont = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            dateTimePicker2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            dateTimePicker2.Format = DateTimePickerFormat.Short;
+            dateTimePicker2.Location = new Point(227, 79);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(343, 38);
+            dateTimePicker2.TabIndex = 27;
+            dateTimePicker2.Value = new DateTime(2023, 10, 17, 14, 34, 1, 0);
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox7.ForeColor = Color.DarkGreen;
+            textBox7.Location = new Point(227, 133);
+            textBox7.Name = "textBox7";
+            textBox7.PlaceholderText = "Försäljningspris";
+            textBox7.Size = new Size(343, 38);
+            textBox7.TabIndex = 26;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(dateTimePicker3);
+            groupBox2.Controls.Add(textBox8);
+            groupBox2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox2.ForeColor = Color.DarkGreen;
+            groupBox2.Location = new Point(1020, 123);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(617, 189);
+            groupBox2.TabIndex = 28;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Inköpnings Historia";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.DarkGreen;
+            label4.Location = new Point(13, 39);
+            label4.Name = "label4";
+            label4.Size = new Size(187, 31);
+            label4.TabIndex = 31;
+            label4.Text = "Inköpningsplats";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox5.ForeColor = Color.DarkGreen;
+            textBox5.Location = new Point(227, 36);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "Inköpningsplats";
+            textBox5.Size = new Size(343, 38);
+            textBox5.TabIndex = 30;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = Color.DarkGreen;
+            label8.Location = new Point(13, 136);
+            label8.Name = "label8";
+            label8.Size = new Size(175, 31);
+            label8.TabIndex = 29;
+            label8.Text = "Inköpningspris";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.ForeColor = Color.DarkGreen;
+            label9.Location = new Point(13, 86);
+            label9.Name = "label9";
+            label9.Size = new Size(205, 31);
+            label9.TabIndex = 28;
+            label9.Text = "Inköpningsdatum";
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.CalendarFont = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            dateTimePicker3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            dateTimePicker3.Format = DateTimePickerFormat.Short;
+            dateTimePicker3.Location = new Point(227, 79);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.Size = new Size(343, 38);
+            dateTimePicker3.TabIndex = 27;
+            dateTimePicker3.Value = new DateTime(2023, 10, 17, 14, 34, 1, 0);
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox8.ForeColor = Color.DarkGreen;
+            textBox8.Location = new Point(227, 133);
+            textBox8.Name = "textBox8";
+            textBox8.PlaceholderText = "Inköpningspris";
+            textBox8.Size = new Size(343, 38);
+            textBox8.TabIndex = 26;
+            // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1739, 847);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(dateTimePicker1);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(tabControl1);
             Controls.Add(textBox1);
             Controls.Add(textBox2);
@@ -277,6 +381,10 @@
             tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -293,13 +401,21 @@
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
-        private DateTimePicker dateTimePicker1;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private Label label1;
-        private Label label2;
-        private Label label3;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
+        private GroupBox groupBox1;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label6;
+        private Label label7;
+        private DateTimePicker dateTimePicker2;
+        private TextBox textBox7;
+        private GroupBox groupBox2;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label8;
+        private Label label9;
+        private DateTimePicker dateTimePicker3;
+        private TextBox textBox8;
     }
 }
