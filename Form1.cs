8fe66f0ed3687e7bdaae7076beed64fe1be18f47
Alvin Alamgir berglund <alvinalamgirberglund@gmail.com>
2023-10-17 13:04:40 +0200

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace ArtCrafter.MasterofCollections
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
